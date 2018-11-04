451d883839f6453554f87914e6604e840ae7d200
shadowman <agascompany@gmail.com>
2018-11-04 19:11:57 +0200

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,9 @@ class MyApp extends StatelessWidget {
 //    );
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
